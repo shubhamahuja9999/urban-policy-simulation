ae7197d3f582fc1ef1465376be77195a021192a6
Fastlad <sarthakbagasi@gmail.com>
2026-08-19 23:22:34 +0530

    feat/perf: optimize landing page performance and UI

--- frontend/landing-page/src/components/ui/ParticleBackground.tsx
+++ frontend/landing-page/src/components/ui/ParticleBackground.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
 import { GPGPUParticles } from "./particles/GPGPUParticles";
 
 export function ParticleBackground() {
@@ -10,16 +9,6 @@ export function ParticleBackground() {
       <Canvas camera={{ position: [0, 0, 5], fov: 60 }} gl={{ antialias: false, alpha: true }}>
         <color attach="background" args={["#000000"]} />
         <GPGPUParticles />
-        
-        {/* Postprocessing for Depth of Field effect */}
-        <EffectComposer multisampling={0}>
-          <DepthOfField 
-            focusDistance={0.0} 
-            focalLength={0.02} 
-            bokehScale={2.5} 
-            height={480} 
-          />
-        </EffectComposer>
       </Canvas>
     </div>
   );
